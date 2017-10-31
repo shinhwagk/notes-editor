cc07a6557bbbd2f3f27ca2ee9a5d06de27288d81
shinhwagk <shanghai_sl@hotmail.com>
2017-10-31 22:59:31 +0800

    save

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -2,14 +2,21 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 const indexObj = JSON.parse((document.getElementById("indexContent") as HTMLTextAreaElement).value)
-console.info(indexObj["labels"][0])
 
-function Category(category) {
-    return <div>{idxContent.aaa["labels"][0]}</div>;
+function Note(props) {
+    const note = Array.from(Array.from({ length: props.cols }).keys()).map(idx => <td>{props.notes[idx]}</td>)
+    return <tr>{note}</tr>
 }
 
-function Notes(idxContent) {
-    return <div>{idxContent.aaa["labels"][0]}</div>;
+
+function Category(props) {
+    const b = props.notes.map(note => <Note notes={note.notes} doc={note.doc} file={note.file} />)
+    return <div>{props.name}<table>{b}</table></div>;
+}
+
+function Notes(categories) {
+    const listItems = categories.map(c => <Category cols={c["cols"]} name={c["name"]} notes={c["notes"]} />);
+    return <div></div>
 }
 
 const element = <Notes aaa={indexObj} />;
