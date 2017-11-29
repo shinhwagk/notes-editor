53b6211a3b70d49038d843040a827110d59f372e
shinhwagk <shanghai_sl@hotmail.com>
2017-11-29 14:31:42 +0800

    save

--- add_id.js
+++ add_id.js
@@ -5,14 +5,22 @@ function addId(nodePath) {
   const indexFile = path.join(nodePath, ".index.json")
   const obj = JSON.parse(fs.readFileSync(indexFile, "UTF-8"))
   const categorys = obj["categorys"]
-  let id = 0
-  for (let i = 0; i <= categorys.length; i++) {
-    const notes = categorys[i]
-    for (let j = 0; j <= notes.length; j++) {
-      fs.renameSync()
+  let id = 1
+  for (let i = 0; i < categorys.length; i++) {
+    const notes = categorys[i]["notes"]
+    for (let j = 0; j < notes.length; j++) {
+      const oldName = path.join(nodePath, `${i}-${j}`)
+      const newName = path.join(nodePath, `n-${id}`)
+      notes[j]["i"] = id
+      fs.renameSync(oldName, newName)
+      id += 1
     }
   }
+  fs.writeFileSync(indexFile, JSON.stringify(obj), "UTF-8")
+  for (const label of obj["labels"]) {
+    const p = path.join(nodePath, label)
+    addId(p)
+  }
 }
-
 
 addId("./")
